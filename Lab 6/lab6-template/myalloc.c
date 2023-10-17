1642eaeb2ba15a5cbeb2bba85223177f2d3af3b6
GoodGuyCuban <marcos.perezperera@gmail.com>
2023-10-17 15:40:15 -0600

    Part 1 passed

--- Lab 6/lab6-template/myalloc.c
+++ Lab 6/lab6-template/myalloc.c
@@ -1,5 +1,6 @@
 #define _DEFAULT_SOURCE
 #include <stddef.h>
+#include <unistd.h>
 #include <sys/mman.h>
 
 
@@ -8,13 +9,18 @@
 node_t *_arena_start;
 
 int myinit(size_t size){
-    int adjusted_size;
+    int adjusted_size = 0;
+    size_t neg = -1;
 
-    if(size <= 0){
+    if(size >= neg){
         return ERR_BAD_ARGUMENTS;
     }
 
-    adjusted_size = size + (4096 - (size % 4096));
+    if(size == getpagesize()){
+        adjusted_size = size;
+    }else{
+        adjusted_size = size + (getpagesize() - (size % getpagesize()));
+    }
 
     _arena_start = mmap(NULL, adjusted_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
     
@@ -23,10 +29,22 @@ int myinit(size_t size){
     _arena_start->bwd = NULL;
     _arena_start->is_free = 1;
 
-    return adjusted_size;
+    return _arena_start->size;
 }
 
 int mydestroy(){
+    if(_arena_start == NULL){
+        return ERR_UNINITIALIZED;
+    }
+
     munmap(_arena_start, _arena_start->size);
-    return 0;
+    _arena_start = NULL;
+
+
+    //check if munmap worked
+    if(_arena_start == NULL){
+        return 0;
+    }else{
+        return ERR_CALL_FAILED;
+    }
 }
